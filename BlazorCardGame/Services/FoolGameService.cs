03251cc56a17830a5ebbdffc1be0211ff3336d17
Stebusha <stebusha@inbox.ru>
2024-12-04 11:50:03 +0300

    transfer logic from component to service

--- BlazorCardGame/Services/FoolGameService.cs
+++ BlazorCardGame/Services/FoolGameService.cs
@@ -5,7 +5,7 @@ namespace BlazorCardGame.Services;
 public class FoolGameService
 {
     private Dictionary<string, bool> _fools = new Dictionary<string, bool>();
-    public ScoreTable scoreTable = new ScoreTable();
+    private ScoreTable scoreTable = new ScoreTable();
     public Player Player { get; set; } = new Player();
     public AIPlayer Opponent { get; set; } = new AIPlayer();
     public Deck Deck { get; set; } = new Deck();
@@ -16,7 +16,103 @@ public class FoolGameService
     public static int CountOfGames { get; set; } = 0;
     public bool FirstTurn { get; set; } = false;
     public int discardCardCount { get; set; } = 0;
+    public bool IsLoaded { get; set; } = false;
+    public bool CanPlay { get; set; } = true;
+    public bool CanDraw { get; set; } = false;
+    public bool CanEndTurn { get; set; } = false;
+    public string PlayButton { get; set; } = "Походить";
+    public string StartButton { get; set; } = "Новая игра";
 
+    //get first trump card in player's hand
+    private Card GetFirstTrump(List<Card> cards)
+    {
+        Card firstTrumpCard = new Card();
+
+        foreach (var card in cards)
+        {
+            if (card.Suit == Deck.s_trumpSuit)
+            {
+                firstTrumpCard = card;
+                break;
+            }
+        }
+
+        return firstTrumpCard;
+    }
+
+    //set start turn number of players use checking min trumps in players' hand or choosing next player after fool
+    private void SetStartTurnNumbers(List<Card> firstTrumpCards)
+    {
+        int number = 1;
+
+        if (Player.IsFool == true)
+        {
+            Opponent.TurnNumber = 1;
+            Player.TurnNumber = 2;
+        }
+        else if (Opponent.IsFool == true)
+        {
+            Player.TurnNumber = 1;
+            Opponent.TurnNumber = 2;
+        }
+
+        else
+        {
+            for (int i = 1; i < firstTrumpCards.Count; i++)
+            {
+                if (firstTrumpCards[i].ImageUrl == "" && firstTrumpCards[i - 1].ImageUrl != "")
+                {
+                    number = 0;
+                }
+                else if (firstTrumpCards[i - 1].ImageUrl == "" && firstTrumpCards[i].ImageUrl != "")
+                {
+                    number++;
+                }
+                else if (firstTrumpCards[i].Rank < firstTrumpCards[i - 1].Rank)
+                {
+                    number++;
+                }
+            }
+
+            if (number == 1)
+            {
+                Player.TurnNumber = 1;
+                Opponent.TurnNumber = 2;
+            }
+            else
+            {
+                Opponent.TurnNumber = 1;
+                Player.TurnNumber = 2;
+            }
+        }
+    }
+
+    //set start turn queue
+    private void SetStartTurnQueue(Player player, AIPlayer opponent)
+    {
+        if (player.TurnNumber == 1)
+        {
+            player.IsAttack = true;
+            opponent.IsAttack = false;
+        }
+        else
+        {
+            player.IsAttack = false;
+            opponent.IsAttack = true;
+        }
+    }
+
+    //rerfesh turn queue for next turn
+    public void RefreshTurnQueue()
+    {
+        if (!Player.Taken && !Opponent.Taken)
+        {
+            Player.IsAttack = !Player.IsAttack;
+            Opponent.IsAttack = !Opponent.IsAttack;
+        }
+    }
+
+    //refill cards in hand
     public void RefillHands()
     {
         if (Deck.CardsAmount <= 6 && Deck.CardsAmount != 0)
@@ -72,68 +168,8 @@ public class FoolGameService
             Opponent.RefillHand(Deck);
         }
     }
-    public void EndCurrentTurn()
-    {
-        WinLose();
 
-        RefreshTurnQueue();
-
-        AttackingCards.Clear();
-        DefendingCards.Clear();
-
-        Player.Taken = false;
-        Opponent.Taken = false;
-
-        foreach (var card in Player.inHand)
-        {
-            card.IsSelected = false;
-        }
-
-        Table.ClearTable();
-
-        RefillHands();
-    }
-    private void WinLose()
-    {
-        if (Deck.CardsAmount == 0)
-        {
-
-            if (Player.inHand.Count == 0 || Opponent.inHand.Count == 0)
-            {
-                gameState = GameState.Finished;
-            }
-
-            if (Player.inHand.Count == 0 && Opponent.inHand.Count != 0)
-            {
-                Console.WriteLine($"Колода закончилась. Конец партии. Победил игрок {Player.Name}.");
-
-                Opponent.IsFool = true;
-
-                if (Opponent.Name != null)
-                {
-                    if (_fools.ContainsKey(Opponent.Name))
-                    {
-                        _fools[Opponent.Name] = Opponent.IsFool;
-                    }
-                }
-
-                int score = 1;
-
-                scoreTable.AddScore(Player.Name, score);
-            }
-            else if (Player.inHand.Count != 0 && Opponent.inHand.Count == 0)
-            {
-                Console.WriteLine($"Колода закончилась. Конец партии. Победил игрок {Opponent.Name}.");
-
-                Player.IsFool = true;
-                _fools[Player.Name] = Player.IsFool;
-
-                int score = 1;
-
-                scoreTable.AddScore(Opponent.Name, score);
-            }
-        }
-    }
+    //load game
     public void LoadGame()
     {
         discardCardCount = 0;
@@ -205,94 +241,358 @@ public class FoolGameService
             FirstTurn = true;
             CountOfGames++;
         }
-    }
 
-    //set start turn queue
-    private void SetStartTurnQueue(Player player, AIPlayer opponent)
-    {
-        if (player.TurnNumber == 1)
+        RefreshPlayButtonName();
+
+        if (Player.IsAttack)
         {
-            player.IsAttack = true;
-            opponent.IsAttack = false;
+            IsLoaded = true;
         }
         else
         {
-            player.IsAttack = false;
-            opponent.IsAttack = true;
+            CanPlay = false;
         }
     }
 
-    //rerfesh turn queue for next turn
-    public void RefreshTurnQueue()
+    private void RefreshStartButton()
     {
+        if (gameState == GameState.Finished || gameState == GameState.JustStarted)
+        {
+            IsLoaded = false;
+            CanPlay = false;
+        }
+        else if (gameState == GameState.InProgress)
+        {
+            IsLoaded = true;
+            CanPlay = true;
+        }
+    }
+
+    private void RefreshPlayButtonName()
+    {
+        if (Player.IsAttack)
+        {
+            if (AttackingCards.Count == 0)
+                PlayButton = "Походить";
+            else
+                PlayButton = "Подкинуть";
+        }
+        else
+        {
+            PlayButton = "Отбиться";
+        }
+    }
+
+    private void CheckNonPlayable()
+    {
+        //checking state of cards for setting enable property for "Play" button
+        var nonPlayable = 0;
+
+        foreach (var card in Player.inHand)
+        {
+            if (!card.IsPlayable)
+            {
+                nonPlayable++;
+            }
+        }
+
+        if (nonPlayable == Player.inHand.Count)
+        {
+            CanPlay = false;
+        }
+    }
+
+    public async Task Turn()
+    {
+        //refresh start properties for game
+        Player.RefreshPlayableForAttack(Table);
+
+        RefreshPlayButtonName();
+
+        //player attack
+        if (Player.IsAttack && Opponent.inHand.Count != 0)
+        {
+            //check first turn discards can't be more than ten
+            if (FirstTurn && Table.Length() != 10)
+            {
+                //choose attacking card
+                Card attackingCard = Player.Attack(Table);
+
+                //if attacking card exists
+                if (attackingCard.ImageUrl != "")
+                {
+                    //add to center table
+                    AttackingCards.Add(attackingCard);
+
+                    RefreshPlayButtonName();
+
+                    //bot's defense
+                    await Task.Delay(500);
+
+                    Card defendingCard = Opponent.Defend(attackingCard, Table);
+
+                    //if defending card exists
+                    if (defendingCard.ImageUrl != "")
+                    {
+                        //add to center table
+                        DefendingCards.Add(defendingCard);
+
+                        //refresh enable property for "EndTurn" button
+                        if (Table.Length() % 2 == 0)
+                        {
+                            CanEndTurn = true;
+                            CanDraw = false;
+                        }
+                    }
+                }
+
+                Player.RefreshPlayableForAttack(Table);
+
+                CheckNonPlayable();
+            }
+            else if (Table.Length() != 12)
+            {
+                //choose attacking card
+                Card attackingCard = Player.Attack(Table);
+
+                //if attacking card exists
+                if (attackingCard.ImageUrl != "")
+                {
+                    //add to center table
+                    AttackingCards.Add(attackingCard);
+
+                    RefreshPlayButtonName();
+
+                    //bot's defense
+                    await Task.Delay(500);
+
+                    Card defendingCard = Opponent.Defend(attackingCard, Table);
+
+                    //if defending card exists
+                    if (defendingCard.ImageUrl != "")
+                    {
+                        //add to center table
+                        DefendingCards.Add(defendingCard);
+
+                        //refresh enable property for "EndTurn" button
+                        if (Table.Length() % 2 == 0)
+                        {
+                            CanEndTurn = true;
+                            CanDraw = false;
+                        }
+                    }
+                }
+
+                Player.RefreshPlayableForAttack(Table);
+
+                CheckNonPlayable();
+            }
+        }
+        //bot's attack
+        else
+        {
+            //getting card for player defense if attacking card exists
+            if (Table.Length() % 2 == 1)
+            {
+                Card attackingCard = AttackingCards.Last();
+
+                if (attackingCard.ImageUrl != "")
+                {
+                    CanDraw = true;
+                    CanPlay = true;
+
+                    Card defendingCard = Player.Defend(attackingCard, Table);
+
+                    //defense if attacking card exists
+                    if (defendingCard.ImageUrl != "")
+                    {
+                        DefendingCards.Add(defendingCard);
+
+                        //refresh enable property for "EndTurn" button
+                        if (Table.Length() % 2 == 0)
+                        {
+                            CanEndTurn = true;
+                            CanDraw = false;
+                            CanPlay = false;
+                        }
+                    }
+                }
+
+                Player.RefreshPlayableForBeat(attackingCard, Table);
+
+                CheckNonPlayable();
+            }
+
+            //all the attacking card defend, not max count of cards on table, player had cards in hand, player not taken
+            if (Table.Length() % 2 == 0 && Table.Length() != 12
+                && Player.inHand.Count != 0 && !Player.Taken)
+            {
+                //bot's attack
+                Card attackingCard = Opponent.Attack(Table);
+
+                await Task.Delay(500);
+
+                if (attackingCard.ImageUrl != "")
+                {
+                    //add attacking card to center table if exists
+                    AttackingCards.Add(attackingCard);
+                    CanDraw = true;
+                    CanPlay = true;
+                }
+                else
+                {
+                    //refresh enable properties for buttons "Play" and "DrawCards"
+                    CanPlay = false;
+                    CanDraw = false;
+                }
+
+                Player.RefreshPlayableForBeat(attackingCard, Table);
+
+                CheckNonPlayable();
+            }
+
+            //player taken
+            if (Player.Taken)
+            {
+                Opponent.IsAttack = true;
+                Player.IsAttack = false;
+            }
+        }
+    }
+    public void StartCurrentTurn()
+    {
+        RefreshPlayButtonName();
+
+        gameState = GameState.InProgress;
+
+        RefreshStartButton();
+
+        //start attack if first turn is bot's
+        if (!Player.IsAttack)
+        {
+            Card attackingCard = Opponent.Attack(Table);
+
+            if (attackingCard.ImageUrl != "")
+            {
+                AttackingCards.Add(attackingCard);
+                CanDraw = true;
+            }
+
+            Player.RefreshPlayableForBeat(attackingCard, Table);
+
+            CheckNonPlayable();
+        }
+        //refresh playable cards if first turn is player's
+        else
+        {
+            Player.RefreshPlayableForAttack(Table);
+        }
+    }
+
+    //cheking win lose condition and set winner
+    private void WinLose()
+    {
+        if (Deck.CardsAmount == 0)
+        {
+
+            if (Player.inHand.Count == 0 || Opponent.inHand.Count == 0)
+            {
+                gameState = GameState.Finished;
+            }
+
+            if (Player.inHand.Count == 0 && Opponent.inHand.Count != 0)
+            {
+                Console.WriteLine($"Колода закончилась. Конец партии. Победил игрок {Player.Name}.");
+
+                Opponent.IsFool = true;
+
+                if (Opponent.Name != null)
+                {
+                    if (_fools.ContainsKey(Opponent.Name))
+                    {
+                        _fools[Opponent.Name] = Opponent.IsFool;
+                    }
+                }
+
+                int score = 1;
+
+                scoreTable.AddScore(Player.Name, score);
+            }
+            else if (Player.inHand.Count != 0 && Opponent.inHand.Count == 0)
+            {
+                Console.WriteLine($"Колода закончилась. Конец партии. Победил игрок {Opponent.Name}.");
+
+                Player.IsFool = true;
+                _fools[Player.Name] = Player.IsFool;
+
+                int score = 1;
+
+                scoreTable.AddScore(Opponent.Name, score);
+            }
+        }
+    }
+
+    //end current turn, refresh game's properties for next turn
+    public void EndCurrentTurn()
+    {
+        //set flag first turn
+        if (FirstTurn)
+        {
+            FirstTurn = false;
+        }
+
+        //take cards if player taken
+        if ((Player.Taken || Table.Length() % 2 == 1) && !Opponent.Taken)
+        {
+            Player.TakeCards(Table);
+        }
+
+        //refresh discard pile if nobody taken
         if (!Player.Taken && !Opponent.Taken)
         {
-            Player.IsAttack = !Player.IsAttack;
-            Opponent.IsAttack = !Opponent.IsAttack;
+            discardCardCount += Table.Length();
         }
-    }
 
-    //get first trump card in player's hand
-    private Card GetFirstTrump(List<Card> cards)
-    {
-        Card firstTrumpCard = new Card();
+        WinLose();
 
-        foreach (var card in cards)
+        RefreshTurnQueue();
+
+        AttackingCards.Clear();
+        DefendingCards.Clear();
+
+        Player.Taken = false;
+        Opponent.Taken = false;
+
+        foreach (var card in Player.inHand)
         {
-            if (card.Suit == Deck.s_trumpSuit)
-            {
-                firstTrumpCard = card;
-                break;
-            }
+            card.IsSelected = false;
         }
 
-        return firstTrumpCard;
-    }
+        Table.ClearTable();
 
-    //set start turn number of players use checking min trumps in players' hand or choosing next player after fool
-    private void SetStartTurnNumbers(List<Card> firstTrumpCards)
-    {
-        int number = 1;
+        RefillHands();
 
-        if (Player.IsFool == true)
+        RefreshPlayButtonName();
+
+        CanEndTurn = false;
+
+        if (Player.IsAttack)
         {
-            Opponent.TurnNumber = 1;
-            Player.TurnNumber = 2;
-        }
-        else if (Opponent.IsFool == true)
-        {
-            Player.TurnNumber = 1;
-            Opponent.TurnNumber = 2;
+            Player.RefreshPlayableForAttack(Table);
         }
 
+        //continue refresh buttons state
+        RefreshStartButton();
+
+        CanPlay = true;
+
+        if (Player.IsAttack)
+        {
+            CanDraw = false;
+        }
         else
         {
-            for (int i = 1; i < firstTrumpCards.Count; i++)
-            {
-                if (firstTrumpCards[i].ImageUrl == "" && firstTrumpCards[i - 1].ImageUrl != "")
-                {
-                    number = 0;
-                }
-                else if (firstTrumpCards[i - 1].ImageUrl == "" && firstTrumpCards[i].ImageUrl != "")
-                {
-                    number++;
-                }
-                else if (firstTrumpCards[i].Rank < firstTrumpCards[i - 1].Rank)
-                {
-                    number++;
-                }
-            }
-
-            if (number == 1)
-            {
-                Player.TurnNumber = 1;
-                Opponent.TurnNumber = 2;
-            }
-            else
-            {
-                Opponent.TurnNumber = 1;
-                Player.TurnNumber = 2;
-            }
+            CanDraw = true;
         }
     }
 }
